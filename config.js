46b3a9d78414b01af40bcf16a4089400d10c1faa
testwpbot <yourownwpbot@gmail.com>
2025-07-10 01:17:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "nBlmhKSA#J0brq2LFegtjwu36HOyYUHHjub7-f3zDQ0ZFUOO-w5s",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94774915917',  // Replace with the owner's phone number
 
 
 
