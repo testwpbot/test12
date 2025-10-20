36ca30e8311a7f7e5e539673e8ce30fa49ea66c8
testwpbot <yourownwpbot@gmail.com>
2025-10-20 19:46:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2ccwzKjK#N_nLp5hfNIZXEFC4nZQv7XcfWEs0eMvQpaWXz1Z3_H0",
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 BOT_OWNER: '94774915917',  // Replace with the owner's phone number
