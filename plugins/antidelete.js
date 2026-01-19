e0e5875361f9c709980437b35bfeda51e1b5af6f
testwpbot <yourownwpbot@gmail.com>
2026-01-19 07:02:07 +0530

    Update antidelete.js

--- plugins/antidelete.js
+++ plugins/antidelete.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
+const { downloadContentFromMessage } = require('gifted-baileys');
 
 const tempFolder = path.join(__dirname, '../temp');
 if (!fs.existsSync(tempFolder)) {
